ad391c1f61fa66b8776beb7960290f20c23d2096
xiaoxiaoluo7 <108709866+xiaoxiaoluo7@users.noreply.github.com>
2022-07-05 15:46:51 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
         System.out.println("zheshibeifen01");
         System.out.println("aaaa");
         System.out.println("push test");
+        System.out.println("poll test");
+        
     }
 }
